5a4e10b212032f8cfcac30c4ae54561a8a51591c
KaitoMuraoka <kaitoboshi@icloud.com>
2024-01-20 18:40:30 +0900

    問題のサンプルコードを作成

--- app/src/main/java/com/example/bottomnavigationsampleapp/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigationsampleapp/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.bottomnavigationsampleapp
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.example.bottomnavigationsampleapp.databinding.ActivityMainBinding
+import com.example.bottomnavigationsampleapp.ui.dashboard.DashboardFragment
+import com.example.bottomnavigationsampleapp.ui.home.HomeFragment
+import com.example.bottomnavigationsampleapp.ui.notifications.NotificationsFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,17 +19,39 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+//        val navView: BottomNavigationView = binding.navView
+//        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+//        navView.setupWithNavController(navController)
+        // ↑があると動く。
+        // onOptionsItemSelectedないに↑のコードが必要？
+    }
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.bottom_nav_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.navigation_home -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_home, HomeFragment())
+                    .commit()
+                true
+            }
+            R.id.navigation_dashboard -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_dashboard, DashboardFragment())
+                    .commit()
+                true
+            }
+            R.id.navigation_notifications -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_notifications, NotificationsFragment())
+                    .commit()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
